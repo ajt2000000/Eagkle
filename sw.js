0532d44b798b27b8669114b89f43f976f9082df9
ajt2000000 <111257318+ajt2000000@users.noreply.github.com>
2024-06-27 15:25:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-
+var cacheName = "MC";
+var appShellFiles = [
+  "classes.js",
+  "classes.js.map",
+  "favicon.png",
+  "index.html",
+  "manifest.json",
+  "sw.js",
+  "jquery.min.js",
+  "assets.epk",
+  "/lang/",
+  "/build/",
+];
