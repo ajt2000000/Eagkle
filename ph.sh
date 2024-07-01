6fa62dcda290607289124f983f02913536e9bb6a
ajt2000000 <111257318+ajt2000000@users.noreply.github.com>
2024-07-01 16:17:07 +0900

    Update ph.sh

--- ph.sh
+++ ph.sh
@@ -1,7 +1,7 @@
 sudo ngrok config add-authtoken 2DZnmKRzmQtZURdDRrCQusfkcnd_6sDTv3t7JQRe2QySoPu8i
 sudo systemctl start ssh.service
-sudo nohup ngrok tunnel --label edge=edghts_2iKGNfleSyyq2qMiJV6UiN3jlhY http://localhost:8081
+sudo ngrok tunnel --label edge=edghts_2iKGNfleSyyq2qMiJV6UiN3jlhY http://localhost:8081 &
 sudo echo NgrokSTART
 cd eaglerproxy-EaglerProxyAAS
 cd build
-sudo node index.js
+sudo node index.js &
