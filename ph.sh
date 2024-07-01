83e492399d6939cd00ba4f4650bf93c41c054491
ajt2000000 <111257318+ajt2000000@users.noreply.github.com>
2024-07-01 16:14:12 +0900

    Update and rename p.sh to ph.sh

--- ph.sh
+++ ph.sh
@@ -1,6 +1,6 @@
 sudo ngrok config add-authtoken 2DZnmKRzmQtZURdDRrCQusfkcnd_6sDTv3t7JQRe2QySoPu8i
 sudo systemctl start ssh.service
-sudo nohup -c ngrok tunnel --label edge=edghts_2iKGNfleSyyq2qMiJV6UiN3jlhY http://localhost:8081
+sudo nohup ngrok tunnel --label edge=edghts_2iKGNfleSyyq2qMiJV6UiN3jlhY http://localhost:8081
 sudo echo NgrokSTART
 cd eaglerproxy-EaglerProxyAAS
 cd build
